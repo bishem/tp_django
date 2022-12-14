35a5ceedde2dcf5c59768f2c9be1af07dbbb987e
Cheshire <aminrsaidali@gmail.com>
2022-12-14 01:53:23 +0100

    fixed script file

--- scripts/clear_compiled.sh
+++ scripts/clear_compiled.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-project_root="$(dirname "$(readlink -f "${BASH_SOURCE}")")"
+project_root="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE}")")")"
 
 find $project_root -name "__pycache__" -print0 | xargs -0 rm -rf
